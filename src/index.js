a38c6167a44a5987361f241cacabaae071fd06e7
Adam Basiak <basiakus@gmail.com>
2018-01-20 14:19:45 +0100

    add devTools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { render, ReactDOM } from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import store from './store';
 import DevTools from './DevTools';
-import routes from './routes;'
 
-ReactDOM.render(
+
+render(
     <Provider store={store}>
     	<div>
-			<Router history={hashHistory} routes={routes} />
+			<Router history={hashHistory}/>
 			<DevTools />
     	</div>
     </Provider>,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,5 @@ import countriesReducer  from './countries-reducer';
 const reducers = combineReducers({
     countriesReducer
 });
-
+store.subscribe(() => console.log(store.getState()));
 export default reducers;
